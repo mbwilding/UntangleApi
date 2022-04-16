85f953cef24c0f472d6a69493ae501b871bd6b0d
mbwilding <mbwilding@gmail.com>
2022-04-17 03:30:00 +0800

    Update

--- ApiTest/Program.cs
+++ ApiTest/Program.cs
@@ -1,3 +1,9 @@
-﻿var untangle = new Untangle.UntangleApi("192.168.1.1:81", "admin", "Password123!!", false, true);
-if (!await untangle.StartAsync()) return;
-Console.ReadLine();
+﻿var untangle = new Untangle.UntangleApi(
+    "192.168.1.1:81",
+    "admin",
+    "Password123!!",
+    false,
+    true);
+if (!await untangle.StartAsync())
+    Console.WriteLine("Start up failed");
+Console.ReadLine(); // Breakpoint here and inspect 'untangle'
